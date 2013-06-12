2c5416a7c55e19e9103c30663a81577118a955ad
Rohit <rohitp.ror@gmail.com>
2013-06-13 00:27:07 +0530

    chema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130130065425) do
+ActiveRecord::Schema.define(:version => 20130610124139) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "product_id"
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "products", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "description"
+    t.float    "value"
+    t.string   "image"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
